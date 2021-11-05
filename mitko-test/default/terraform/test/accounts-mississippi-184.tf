3e107f13980c7b2651b76975135b50067a9fd21f
Mitko Georgiev <mitko.georgiev@cycloid.io>
2021-11-05 16:14:42 +0000

    Create Config Repository by mitko-config

---
+++ mitko-test/default/terraform/test/accounts-mississippi-184.tf
@@ -0,0 +1,9 @@
+module "tf-accounts-mississippi-184" {
+  source = "module-accounts-mississippi-184"
+
+  # env - type: string
+  # env = "[PLACEHOLDER]"
+
+  # project - type: string
+  # project = "[PLACEHOLDER]"
+}
